c7bc7ce22c98b0feb7f06b1ad11856b84c3f733f
prettydev <creditcoder@hotmail.com>
2021-11-03 16:32:49 -0700

    modify x->w in cache.py

--- zerver/lib/cache.py
+++ zerver/lib/cache.py
@@ -92,7 +92,7 @@ def get_or_create_key_prefix() -> str:
     filename = os.path.join(settings.DEPLOY_ROOT, "var", "remote_cache_prefix")
     print("filename is:::::::::::::::::::"+filename)
     try:
-        with open(filename, "x") as f:
+        with open(filename, "w") as f:
             prefix = secrets.token_hex(16) + ":"
             f.write(prefix + "\n")
 
