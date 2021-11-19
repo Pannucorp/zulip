0df369801f2b236466ecdcdb565c2b1b1bafa2aa
prettydev <creditcoder@hotmail.com>
2021-11-18 18:14:19 -0800

    add # type: ignore[no-untyped-def]  in pwa

--- zerver/views/pwa.py
+++ zerver/views/pwa.py
@@ -10,7 +10,7 @@ from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 import zproject.settings
 
-def service_worker(request) -> HttpResponse:
+def service_worker(request):# type: ignore[no-untyped-def] 
     # response = HttpResponse(open(zproject.settings.PWA_SERVICE_WORKER_PATH).read(), content_type='application/javascript')
     response = render(request, 'pwa/serviceworker.js',content_type='application/x-javascript')
     return response
@@ -23,11 +23,11 @@ def service_worker(request) -> HttpResponse:
 #         if setting_name.startswith('PWA_')
 #     }, content_type='application/json')
 
-def offline(request) -> HttpResponse:
+def offline(request):# type: ignore[no-untyped-def] 
     return render(request, 'pwa/offline.html')
 
 
-def my_page(request) -> HttpResponse:
+def my_page(request):# type: ignore[no-untyped-def] 
     routes = {
         'Home': reverse('home'),
         'Say hi': reverse('say_something', kwargs={'key': 'hi'}),
@@ -41,7 +41,7 @@ def my_page(request) -> HttpResponse:
     return render(request, 'pwa/my_page.html', context={'routes': routes})
 
 
-def say_something(request, key) -> HttpResponse:
+def say_something(request, key):# type: ignore[no-untyped-def] 
     things_to_say = {
         'hi': 'Hello world',
         'bye': 'Have a nice day',
@@ -53,7 +53,7 @@ def say_something(request, key) -> HttpResponse:
     return render(request, 'pwa/say_something.html', context={'thing': things_to_say[key]})
 
 
-def random_response(request) -> HttpResponse:
+def random_response(request):# type: ignore[no-untyped-def] 
     response_time_ms = random.choice((0, 10, 50, 100, 1_000, 10_000))
     response_time = response_time_ms / 1_000
     print(f'Selected response time {response_time}')
@@ -61,12 +61,12 @@ def random_response(request) -> HttpResponse:
     return render(request, 'pwa/random_response.html', context={'response_time': response_time})
 
 
-def fill_dynamic_cache(request, id) -> HttpResponse:
+def fill_dynamic_cache(request, id):# type: ignore[no-untyped-def] 
     return render(request, 'pwa/fill_dynamic_cache.html', context={'id': id})
 
 
 @never_cache
-def must_not_cache(request) -> HttpResponse:
+def must_not_cache(request):# type: ignore[no-untyped-def] 
     return render(request, 'pwa/must_not_cache.html', context={'requested_at': timezone.now()})
 
 
@@ -75,7 +75,7 @@ class ServiceWorkerView(TemplateView):
     content_type = 'application/javascript'
     name = 'sw.js'
 
-    def get_context_data(self, **kwargs) -> HttpResponse:
+    def get_context_data(self, **kwargs):# type: ignore[no-untyped-def] 
         print(f'Selected response time::::::::::::::::::')
         return {
             'version': '1.0.1',
