6335dd4b7f737381877f43d89ab8da12685ef9ae
prettydev <creditcoder@hotmail.com>
2021-11-20 08:59:11 +0800

    add pwa icons, refine wrong icon path

--- templates/pwa/serviceworker.js
+++ templates/pwa/serviceworker.js
@@ -1,25 +1,14 @@
 var staticCacheName = "django-pwa-v" + new Date().getTime();
 var filesToCache = [
     '/offline/',
-    '/static/css/django-pwa-app.css',
-    '/static/images/icons/icon-72x72.png',
-    '/static/images/icons/icon-96x96.png',
-    '/static/images/icons/icon-128x128.png',
-    '/static/images/icons/icon-144x144.png',
-    '/static/images/icons/icon-152x152.png',
-    '/static/images/icons/icon-192x192.png',
-    '/static/images/icons/icon-384x384.png',
-    '/static/images/icons/icon-512x512.png',
-    '/static/images/icons/splash-640x1136.png',
-    '/static/images/icons/splash-750x1334.png',
-    '/static/images/icons/splash-1242x2208.png',
-    '/static/images/icons/splash-1125x2436.png',
-    '/static/images/icons/splash-828x1792.png',
-    '/static/images/icons/splash-1242x2688.png',
-    '/static/images/icons/splash-1536x2048.png',
-    '/static/images/icons/splash-1668x2224.png',
-    '/static/images/icons/splash-1668x2388.png',
-    '/static/images/icons/splash-2048x2732.png'
+    '/static/icons/icon-72x72.png',
+    '/static/icons/icon-96x96.png',
+    '/static/icons/icon-128x128.png',
+    '/static/icons/icon-144x144.png',
+    '/static/icons/icon-152x152.png',
+    '/static/icons/icon-192x192.png',
+    '/static/icons/icon-384x384.png',
+    '/static/icons/icon-512x512.png',
 ];
 
 self.addEventListener("install", event => {
@@ -27,21 +16,23 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(staticCacheName)
             .then(cache => {
+                console.log("service workder installed!");
                 return cache.addAll(filesToCache);
-            })
+            }).catch(err=>console.log("service worker failed:", err))
     )
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
+            console.log("service workder activated!");
             return Promise.all(
                 cacheNames
                     .filter(cacheName => (cacheName.startsWith("django-pwa-")))
                     .filter(cacheName => (cacheName !== staticCacheName))
                     .map(cacheName => caches.delete(cacheName))
             );
-        })
+        }).catch(err=>console.log("service worker activate failed:", err))
     );
 });
 
@@ -49,9 +40,11 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
+                console.log("fetch succeed!")
                 return response || fetch(event.request);
             })
-            .catch(() => {
+            .catch((err) => {
+                console.log("fetch failed;", err);
                 return caches.match('/offline/');
             })
     )
